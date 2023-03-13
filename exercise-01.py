654abd6ffe21934359240deeb51c5150328bcadc
nnalalala <fatkhuriyaninala@gmail.com>
2023-03-13 18:11:16 +0700

    mengerjakan exerxise-01

---
+++ exercise-01.py
@@ -0,0 +1,10 @@
+nama = "Nala Fatkhuriyani"
+nim = "I0322092"
+kampunghalaman = "kebumen"
+visualisasi = "Visualisasi kampung halaman di kota Kota Kebumen terdapat taman kecil ditengahnya dan beberapa pohon yang memberikan suasana asri"    
+harapan = "harapan setelah lulus adalah mendapatkan pekerjaan yang memungkinkan untuk terus belajar dan mengembangkan keterampilan teknis yang diperlukan untuk sukses di bidang Teknik Industri"
+print("Namaku" " " + nama)
+print("dengan NIM" " " + nim)
+print("Kampung halamanku di" " " + kampunghalaman)
+print(visualisasi)
+print(harapan)
